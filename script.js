b85ec4db31133fec05e024e25c10b208415a197e
Rafael Morales de Lima <116084372+rafaelmorales2010@users.noreply.github.com>
2026-09-08 15:06:42 +0000

    variaveis

---
+++ script.js
@@ -0,0 +1,10 @@
+let nome = "João";
+let idade = 16;
+let cidade = "Marilena";
+let altura = 1.72;
+
+console.log("Nome: "+nome);
+console.log("Idade: "+idade);
+console.log("Altura: "+altura);
+console.log("Cidade: "+cidade);
+
